17710ad56cd9fcd070968dbe90e5da668eeb7a45
Jason C. Leach <jason.leach@fullboar.ca>
2023-03-13 10:31:04 -0700

    chore: reorg test scripts
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

---
+++ run-2157.ts
@@ -0,0 +1,18 @@
+ #!/bin/bash
+ 
+ OK=0
+ FAIL=0
+ while true; do 
+  rm -rf ~/.indy_client/
+  ./node_modules/.bin/ts-node ./1365.ts -m $PROD_MEDIATOR_URL 2>&1 1>/dev/null
+  if [ $? -ne 0 ]; then
+    ((FAIL++))
+  else 
+    ((OK++))
+  fi
+  
+  echo "Results: Total $((FAIL + OK)), OK = $OK, FAIL = $FAIL"
+  
+  sleep 3
+  
+done
